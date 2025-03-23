3f7051f00bad8052a4dee9f831d450bad9a4db12
CR7 <542539719@qq.com>
2025-03-23 18:51:47 +0800

    test

--- src/CastleGame/Play.java
+++ src/CastleGame/Play.java
@@ -6,19 +6,6 @@ public class Play {
     public static void main(String[] args) {
         RunGame runGame = new RunGame();
         runGame.showWelcome();
-        Scanner input = new Scanner(System.in);
-        while (true) {
-            String s = input.nextLine();
-            String[] array = s.split(" ");
-            if (array[0].equals("help")) {
-                runGame.showHelp();
-            } else if (array[0].equals("go")) {
-                runGame.showRoom(array[1]);
-            } else if (array[0].equals("bye")) {
-                break;
-            }
-        }
-        System.out.println("游戏结束！");
-        input.close();
+        runGame.play();
     }
 }
